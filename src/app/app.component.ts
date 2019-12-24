7693582f54d095592381b6b0cb556d0b7d9443d9
FV3zy9eKBT <panyeu120@gmail.com>
2019-12-25 03:16:53 +0700

    push lên chứ không biết nói gì luôn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.htmlasdasdas',
+  styleUrls: ['./app.component.scssasdasl;dkas;ldk']
 })
 export class AppComponent {
   title = 'WebApp';
